28fdf816ed7a2f0265d3affa5c73bc1dea75d852
Satoshi Fukutomi <info@fuktommy.com>
2011-02-12 15:19:26 +0000

    配色を新しい掲示板に合わせる。
    git-svn-id: http://shingetsu.info/svn/main/shingetsu.info/trunk@1595 50d94fb2-023c-46d5-88bd-75904fdc1e23

---
+++ template/adsmini.h
@@ -0,0 +1,11 @@
+<script type="text/javascript"><!--
+  amazon_ad_tag = "fuktommysstor-22";
+  amazon_ad_width = "180";
+  amazon_ad_height = "150";
+  amazon_ad_logo = "hide";
+  amazon_color_border = "808080";
+  amazon_color_background = "FFFFFF";
+  amazon_color_link = "0000EE";
+  amazon_color_logo = "FFFFFF";
+//--></script>
+<script type="text/javascript" src="http://www.assoc-amazon.jp/s/ads.js"></script>
