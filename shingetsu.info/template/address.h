ecb1a677fe1e4a0da0e85a7f0554ca612aa35bd9
Satoshi Fukutomi <info@fuktommy.com>
2014-02-01 19:06:39 +0900

    朔3.15.0

--- shingetsu.info/template/address.h
+++ shingetsu.info/template/address.h
@@ -1,4 +1,4 @@
-<div class="address">Copyright&copy; 2003-2013
+<div class="address">Copyright&copy; 2003-2014
 <a href="http://www.shingetsu.info/">shinGETsu Project</a>.
 All Rights Reserved.<br />
 <a href="mailto:webmaster@shingetsu.info">webmaster@shingetsu.info</a>
