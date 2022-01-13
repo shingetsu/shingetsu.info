d00a80257e1e8302f81f0deb585ef57e58842647
Satoshi Fukutomi <info@fuktommy.com>
2022-01-13 18:40:55 +0900

    朔不安定版

--- shingetsu.info/template/address.h
+++ shingetsu.info/template/address.h
@@ -1,4 +1,4 @@
-<div class="address">Copyright&copy; 2003-2021
+<div class="address">Copyright&copy; 2003-2022
 <a href="https://shingetsu.info/">shinGETsu Project</a>.
 All Rights Reserved.<br />
 <a href="mailto:webmaster@shingetsu.info">webmaster@shingetsu.info</a>
