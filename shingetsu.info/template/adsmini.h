ee166a2564d4d75dbfe7f95eda962767ab240cca
Satoshi Fukutomi <info@fuktommy.com>
2022-11-29 09:45:31 +0900

    朔不安定版

--- shingetsu.info/template/adsmini.h
+++ shingetsu.info/template/adsmini.h
@@ -1,17 +1,1 @@
-<script type='text/javascript'>
-var amzn_wdgt={widget:'MyFavorites'};
-amzn_wdgt.tag='shingetsubbs-22';
-amzn_wdgt.columns='1';
-amzn_wdgt.rows='1';
-amzn_wdgt.title='';
-amzn_wdgt.width='150';
-amzn_wdgt.ASIN='B08BGBZT16,B07QMYTRR9,B06W5528D5,B074CGZ1XN,B01HAWP672,4040685520,B01N3XV7YK,B07P83Y4K2,B07YVGD8WG,B08KPXXTH6,B084KKPJ2P,B084QJGSVH';
-amzn_wdgt.showImage='True';
-amzn_wdgt.showPrice='True';
-amzn_wdgt.showRating='False';
-amzn_wdgt.design='2';
-amzn_wdgt.colorTheme='White';
-amzn_wdgt.marketPlace='JP';
-</script>
-<script type='text/javascript' src='https://wms-fe.amazon-adsystem.com/20070822/JP/js/AmazonWidgets.js'>
-</script>
+<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&amp;bc1=000000&amp;IS2=1&amp;bg1=FFFFFF&amp;fc1=000000&amp;lc1=0000FF&amp;t=shingetsubbs-22&amp;language=ja_JP&amp;o=9&amp;p=8&amp;l=as4&amp;m=amazon&amp;f=ifr&amp;ref=as_ss_li_til&amp;asins=B0B5DFG6ZP&amp;linkId=c08e3ba4b81ca96e62a0438f40a49fe6"></iframe>
